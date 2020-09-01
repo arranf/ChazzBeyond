06c15684bbb29a0519ffe42bdd0ca7facbbb6582
Arran France <arran@arranfrance.com>
2020-09-01 19:53:35 +0100

    Readme and build tweaks

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # ChazzBeyond
 
-```
+A simple browser extension for rolling dice using DnD Beyond and sharing the results via Discord.
+
+```sh
 yarn build
 ```
+
+## Usage
+
+Using the options window save the configuration provided by the Discord bot (use `!beyond` in the chat).
+Then log into your D&D Beyond account, and roll dice - the results should appear in Discord.
--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('html', function () {
 
 //copy vendor scripts and uglify all other scripts, creating source maps
 gulp.task('js', () => {
-	return gulp.src("src/js///*.js")
+	return gulp.src("src/js//*.js")
 		.pipe(babel({
 			presets: ['@babel/preset-env']
 		}))
@@ -50,23 +50,17 @@ gulp.task('css', function () {
 });
 
 //build ditributable and sourcemaps after other tasks completed
-gulp.task('zip', gulp.series('html', 'js', 'css', 'copy', () => {
-	const manifest = require('./src/manifest'),
-		distFileName = manifest.name + ' v' + manifest.version + '.zip',
-		mapFileName = manifest.name + ' v' + manifest.version + '-maps.zip';
-	//collect all source maps
-	gulp.src('build/scripts//*.map')
-		.pipe(zip(mapFileName))
-		.pipe(gulp.dest('build'));
-	//build distributable extension
-	return gulp.src(['build/', '!build/scripts/**/*.map'])
+gulp.task('zip', function () {
+	const manifest = require('./src/manifest');
+	const distFileName = manifest.name + ' v' + manifest.version + '.zip';
+	return gulp.src('build/')
 		.pipe(zip(distFileName))
 		.pipe(gulp.dest('build'));
-}));
+});
 
 
-+ gulp.task('default', gulp.series('clean', 'zip'), () => {
-	gulp.watch(['./src/css/**/*.css'], ['css']);
-	gulp.watch(['./src/js/**/*.mjs'], ['js']);
-	gulp.watch(['./src/html/**/*.html'], ['html']);
+gulp.task('default', gulp.series('clean', gulp.parallel('html', 'js', 'css', 'copy'), 'zip'), () => {
+	gulp.watch(['./src/css//*.css'], ['css']);
+	gulp.watch(['./src/js//*.js'], ['js']);
+	gulp.watch(['./src/html//*.html'], ['html']);
 });
